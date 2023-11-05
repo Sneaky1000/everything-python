fb85c0e9d4c54a2f82fed0fd5038e804bfa46a82
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-05 01:41:10 -0800

    Add: Math operations

---
+++ math_operations.py
@@ -0,0 +1,33 @@
+# Math operations
+
+# Addition
+print(5 + 10) # Output: 15
+
+# Subtraction
+print(10 - 5) # Output: 5
+
+# Multiplication
+print(5 * 2) # Output: 10
+
+# Division
+print(5 / 2) # Output: 2.5
+
+# Modulo (remainder)
+print(5 % 2) # Output: 1
+
+# Exponentiation (power of x)
+print(5 ** 2) # Output: 25
+
+# Floor division (Lowest truncated value - no decimals)
+print (5 // 2) # Output: 2
+
+# Booleans (work the same as any other language)
+print(1 < 2) # Output: true
+print(1 <= 2) # Output: true
+print(1 > 2) # Output: false
+print(1 >= 2) # Output: false
+print(1 == 2) # Output: false
+
+# Other notes
+# -----------
+# 1. Parenthesis work the same as any other calculator
