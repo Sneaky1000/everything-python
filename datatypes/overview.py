a09b5a06e389cd497442afb227be7ae9097547a8
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-05 23:45:45 -0800

    Add: Data types overview

---
+++ datatypes/overview.py
@@ -0,0 +1,39 @@
+# Data types
+
+'''
+There are several data types in Python. Here are the built-in types:
+
+1. Numeric types
+  a. Integer: `int`
+  b. Float: `float`
+  c. Complex: `complex`
+
+2. Text types
+  a. String: `str`
+
+3. Sequence types
+  a. List: `list`
+  b. Tuple: `tuple`
+  c. Range: `range`
+
+4. Mapping types
+  a. Dictionary: `dict`
+
+5. Set types
+  a. Set: `set`
+  b. Frozen Set: `frozenset`
+
+6. Boolean types
+  a. Boolean: `bool`
+
+7. Binary types
+  a. Bytes: `bytes`
+  b. Byte Array: `bytearray`
+  c. Memory View: `memoryview`
+
+8. None types (null/nil)
+  a. None: `none`
+
+This file will go over most of the basic data types that are used most commonly
+
+'''
