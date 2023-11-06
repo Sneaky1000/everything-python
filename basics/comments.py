087aad4569b63600835733ea110aaa17a9f357c4
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-05 18:13:21 -0800

    Add: Comments

---
+++ basics/comments.py
@@ -0,0 +1,10 @@
+# Comments
+
+# Comments are very straightforward in Python
+# Single line comments are initalized with a '#' character
+
+'''
+There are no real multi-line comments in Python.
+However, since string literals are ignored in Python,
+You can use them like this to create a multi-line comment.
+'''
