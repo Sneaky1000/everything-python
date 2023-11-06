fdb30004527e39e670dcbd3a7865611bfdff825f
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-05 17:28:25 -0800

    Add: Built-in functions

---
+++ basics/built_in_functions.py
@@ -0,0 +1,16 @@
+# Functions
+
+# There are several built-in functions which can be found here: https://docs.python.org/3/library/functions.html
+# Some, such as len() and print(), are used often and should be remembered
+# Built-in functions take in arguments that are passed and return an output based on the argument passed
+
+# This file will go over the details of the built-in function 'print()'
+# All other built-in functions and their details/documentation can be found at the link above
+print(len('some string')) # Output: 11 (spaces are counted as a character and thus are added to the calculation)
+
+# print() accepts an unlimited number of arguments separated by a comma
+print(1, 2, 3, 'etc') # Output: 1 2 3 etc
+
+# Other notes
+# -----------
+# 1. print() outputs to the console/terminal
