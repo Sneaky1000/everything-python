9fe75288216a1fa876f84e91a7487656595d6381
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-06 22:38:22 -0800

    Add: Numbers

---
+++ datatypes/numbers.py
@@ -0,0 +1,12 @@
+# Numbers
+
+# Floating point numbers should be avoided if possible because decimals
+# such as x.1 or x.3 are not exactly representable in a binary floating point number
+print(1.1 * 3) # Output: 3.3000000000000003
+
+
+
+'''
+That issue is common with other programming languages but besides this,
+numbers are similar to other languages in the way they are used and function.
+'''
