cd0a34bd26fd2070aefc491c63bf8c600284c84a
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-07 21:31:36 -0800

    Update: Numbers

--- datatypes/numbers.py
+++ datatypes/numbers.py
@@ -4,7 +4,11 @@
 # such as x.1 or x.3 are not exactly representable in a binary floating point number
 print(1.1 * 3) # Output: 3.3000000000000003
 
+# Long decimal numbers are considered to be floats in Python
 
+# Numbers can also be integers such as 1 or 53 or even 54049325 (wholr numbers)
+
+# There are also complex numbers such as 2 + 4i but a package called cmath is required
 
 '''
 That issue is common with other programming languages but besides this,
