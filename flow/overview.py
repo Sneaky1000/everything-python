440944502b91e662bf00a2946e29abbdcd18f750
Chase Wheeler <cwheeler0204@gmail.com>
2023-11-07 22:51:44 -0800

    Add: Flow overview

---
+++ flow/overview.py
@@ -0,0 +1,21 @@
+# Flow overview
+
+'''
+Python sees chunks of code based on indentation, not brackets (ew...).
+
+There are seven major ways that determine the flow of the code in Python
+  1. If, elif, else - Runs code based on if conditions are satisfied or not
+  2. Match - If statements for more specific statements (switch statements)
+  3. While - Repeats code as long as a condition is true
+  4. For - Runs code for every item in a container (generally)
+  5. Async - Runs code concurrently with other code to execute functions faster (`asyncio` module)
+  6. Multithreading - Runs code parallel with other code using threads (`threading` module)
+  7. Multiprocessing - Runs code parallel with other code using processes (`multiprocessing` module)
+  
+Commands like 'break' and 'next' can also be used to have even more control.
+
+This folder will go over the first *four* ways that alter the flow of the code as they are built in and
+don't require an external module (even though these modules are built-in and just need to be imported).
+'''
+
+
